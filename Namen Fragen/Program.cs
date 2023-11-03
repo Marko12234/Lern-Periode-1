25de1c9c6a6320286ceeae633878a57204e32e80
Marko12234 <142885351+Marko12234@users.noreply.github.com>
2023-11-03 11:10:19 +0100

    Add files via upload

---
+++ Namen Fragen/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Namen_Fragen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie Ihren Namen ein:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Ihr Name ist: " + userInput);
+            Console.Read();
+           
+        }
+    }
+}
